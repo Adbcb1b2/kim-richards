5725b6dd3db252e07891cb69a05c593f0c9c215b
Kim Richards <richards.kimc@gmail.com>
2024-11-04 19:13:31 +0000

    Fixed hamburger nav

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // Open the burger menu when clicked
 function toggleMenu(event) {
-
-    event.preventDefault(); // Prevent default action of the event
-    // Get elements, assingn to variables
+    event.preventDefault(); // Prevent default action of the event only for the hamburger icon
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
 
     // Add open class to elements, can then target in CSS
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+}
+
+// Close the menu if a link is clicked
+function closeMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    menu.classList.remove("open");
+    icon.classList.remove("open");
 }
 
 // Close the menu if screen is resized to desktop width and back to mobile
